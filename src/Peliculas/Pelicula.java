5ad06fe5e9762083e7884d59c17e3fb91f13e632
AndreyRuRO28 <anrugamaro@est.utn.ac.cr>
2023-12-04 16:06:25 -0600

    Se agregan clases
    Clases pelicula, prestamo, entidad

--- src/Peliculas/Pelicula.java
+++ src/Peliculas/Pelicula.java
@@ -4,10 +4,65 @@
  */
 package Peliculas;
 
+import Categorias.Categoria;
+import entidad.Entidad;
+import java.util.Objects;
+
 /**
  *
  * @author Pablo Rod
  */
-public class Pelicula {
+public class Pelicula extends Entidad {
+
+    private String categoria;
+
+    public Pelicula( String codigo, String nombre,String categoria) {
+        super(codigo, nombre);
+        this.categoria = categoria;
+    }
     
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Pelicula(String codigo, String nombre) {
+        super(codigo, nombre);
+    }
+
+    @Override
+    public String toString() {
+        return "codigo: " + codigo + " nombre: " + nombre + " categoria: " + categoria; 
+    }
+
+   
+
+   @Override
+public int hashCode() {
+    int hash = 7;
+    hash = 31 * hash + Objects.hashCode(this.codigo);
+    hash = 31 * hash + Objects.hashCode(this.nombre);
+    hash = 31 * hash + Objects.hashCode(this.categoria);
+    return hash;
+}
+
+@Override
+public boolean equals(Object obj) {
+    if (this == obj) {
+        return true;
+    }
+    if (obj == null || getClass() != obj.getClass()) {
+        return false;
+    }
+    final Pelicula other = (Pelicula) obj;
+    return Objects.equals(this.codigo, other.codigo)
+            && Objects.equals(this.nombre, other.nombre)
+            && Objects.equals(this.categoria, other.categoria);
+}
+
+
 }
