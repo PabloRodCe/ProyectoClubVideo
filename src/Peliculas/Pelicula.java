952b983c27880fd7b61f9d2f5701d97f7a32bb05
Pablo Rodriguez Centeno <jorodriguezce@est.utn.ac.cr>
2023-12-04 19:26:31 -0600

    se actualizacion
    se actualizan las ventanas GUI

--- src/Peliculas/Pelicula.java
+++ src/Peliculas/Pelicula.java
@@ -34,7 +34,6 @@ public class Pelicula extends Entidad {
         super(codigo, nombre);
     }
 
-    @Override
     public String toString() {
         return "codigo: " + codigo + " nombre: " + nombre + " categoria: " + categoria; 
     }
